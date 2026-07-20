1d29cc2354d5862f6b62e74bd192b1d34e3c7b0c
kimoachaia-ai <kimoachaia@gmail.com>
2026-07-20 12:46:43 +0300

    some edits for deployments test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,13 @@
-import { RouterProvider } from "react-router";
-import { router } from "./routes";
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter> {}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
+  );
 }
